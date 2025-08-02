5e3332458e5d48ec80556738d0b5ad2ab084c05c
FriyadDev <riyadhasanblue2323@gmail.com>
2025-08-03 01:54:19 +0600

    feat: error and not-found page added

---
+++ src/app/error.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div className="flex items-center min-h-screen px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
+      <div className="w-full space-y-6 text-center">
+        <div className="space-y-3">
+          <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">
+            Oops! Something Went Wrong
+          </h1>
+          <p className="text-gray-500">
+            Looks like we've hit a snag in the digital realm.
+          </p>
+        </div>
+        <Button size="lg" onClick={reset} className="cursor-pointer">
+          Try again
+        </Button>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function NotFound({}) {
+  const router = useRouter();
+  return (
+    <div className="flex items-center min-h-screen px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
+      <div className="w-full space-y-6 text-center">
+        <div className="space-y-3">
+          <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">
+            404 Page Not Found
+          </h1>
+          <p className="text-gray-500 my-4">
+            Sorry, we couldn&#x27;t find the page you&#x27;re looking for.
+          </p>
+        </div>
+
+        <Button onClick={router.back} size="lg" className="cursor-pointer">
+          Return to website
+        </Button>
+      </div>
+    </div>
+  );
+}
